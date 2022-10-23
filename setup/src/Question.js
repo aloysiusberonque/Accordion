ac226c853edeeb187874dc72c487b8ec73988790
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-10-23 16:34:37 +0800

    Created questions and info

--- setup/src/Question.js
+++ setup/src/Question.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
-  const [showInfo, setSohwInfo] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn" onClick={setSohwInfo(!showInfo)}>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 };
